78c68070251c885a7faae535ed2b5f5d52c62b45
gollilagollilagollila <b1845350@planet.kanazawa-it.ac.jp>
2020-02-27 09:22:56 +0900

    ver 2.0

--- src/transcribe.py
+++ src/transcribe.py
@@ -1,31 +1,6 @@
-#!/usr/bin/env python
+# !/usr/bin/env python
 
-# Copyright 2017 Google Inc. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-"""Google Cloud Speech API sample application using the streaming API.
-
-NOTE: This module requires the additional dependency `pyaudio`. To install
-using pip:
-
-    pip install pyaudio
-
-Example usage:
-    python transcribe_streaming_mic.py
-"""
-
-# [START speech_transcribe_streaming_mic]
+# [START transcribe]
 from __future__ import division
 
 import re
@@ -120,7 +95,7 @@ class MicrophoneStream(object):
 
             yield b''.join(data)
 
-class CallApi(MicrophoneStream):
+class CallApi(object):
     def __init__(self):
         rospy.init_node('speech_recog')
         rospy.loginfo("Init_node 'speech_recog' ")
@@ -237,4 +212,5 @@ if __name__ == '__main__':
     loop_time = 0
     call = CallApi()
     call.conversation()
-# [END speech_transcribe_streaming_mic]
+
+# [END transcribe]
